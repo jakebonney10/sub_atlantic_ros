e45dc67dfd529af48384ebfc1d6f28d4554c77ce
JakeBonney <jakebonney10@gmail.com>
2024-01-25 11:07:55 -0500

    Added parameter for update rate in Hz and added to valveCmdcallback

--- sub_atlantic/src/valve_pack.cpp
+++ sub_atlantic/src/valve_pack.cpp
@@ -16,22 +16,28 @@ ValvePackDriver::ValvePackDriver() : Node("valve_pack_driver"), serial(io) {
 
 void ValvePackDriver::initializeParameters() {
   this->declare_parameter<int>("baud_rate", 38400);
-  this->declare_parameter<std::string>("device_address", "/dev/ttyUSB0");
-  this->declare_parameter<int>("connect_timeout", 10);
+  this->declare_parameter<std::string>("serial_port", "/dev/ttyUSB0");
+  this->declare_parameter<int>("update_rate_hz", 40);
+  this->declare_parameter<int>("connect_timeout_s", 10);
 
   int baud_rate = this->get_parameter("baud_rate").as_int();
-  std::string device_address = this->get_parameter("device_address").as_string();
-  int connect_timeout = this->get_parameter("connect_timeout").as_int();
+  std::string serial_port = this->get_parameter("serial_port").as_string();
+  int update_rate = this->get_parameter("update_rate_hz").as_int();
+  int connect_timeout = this->get_parameter("connect_timeout_s").as_int();
 }
 
 void ValvePackDriver::valveCmdCallback(const sub_atlantic_interfaces::msg::ValveCmd::SharedPtr msg) {
-  // Implement callback functionality for subscriber here
-  // ...
-}
+  std_msgs::Header header = msg->header;
+  int32_t valve_id = msg->valve_id;
+  bool is_on = msg->is_on;
+  bool is_open_loop_control = msg->is_open_loop_control;
+  double current_set_point = msg->current_set_point;
+  
+  // TODO: Implement the logic for handling the ValveCmd message
 
-bool ValvePackDriver::connectSerial(int baud_rate, std::string device_address) {
+bool ValvePackDriver::connectSerial(int baud_rate, std::string serial_port) {
   try {
-      serial.open(device_address);
+      serial.open(serial_port);
       serial.set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
       serial.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
       serial.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
@@ -65,9 +71,8 @@ void ValvePackDriver::changeBit(uint8_t& byte, uint8_t bit, bool value) {
 uint8_t ValvePackDriver::findSubstitutionCharacter(const std::vector<uint8_t>& frame) {
     uint8_t possibleSubChar = 0x00;
     do {
-        if (possibleSubChar != 0xAA) {
-            // Check if possibleSubChar exists in the frame
-            if (std::find(frame.begin(), frame.end(), possibleSubChar) == frame.end()) {
+        if (possibleSubChar != 0xAA) { 
+            if (std::find(frame.begin(), frame.end(), possibleSubChar) == frame.end()) { 
                 return possibleSubChar; // Found a suitable substitution character
             }
         }
@@ -77,7 +82,6 @@ uint8_t ValvePackDriver::findSubstitutionCharacter(const std::vector<uint8_t>& f
     throw std::runtime_error("No suitable substitution character found");
 }
 
-
 void ValvePackDriver::replaceSubstitutionCharacter(std::vector<unsigned char>& data, 
                                                    unsigned char characterToReplace, 
                                                    unsigned char substitutionCharacter) {
@@ -86,7 +90,7 @@ void ValvePackDriver::replaceSubstitutionCharacter(std::vector<unsigned char>& d
 
 std::vector<uint8_t> ValvePackDriver::standardControlMsg(uint8_t pwm1_8, uint8_t pwm9_16, uint8_t pwm_open_loop) {
   std::vector<uint8_t> msg; 
-  msg.push_back(0x00); // Index 0: Substitution Character TODO: placeholder for now
+  msg.push_back(0x00); // Index 0: Substitution Character - TODO: placeholder for now
   msg.push_back(0x01); // Index 1: Message ID - always 0x01 for standard control message
   msg.push_back(0x00); // Index 2: Control Serials and Message Address for second board - not implemented, set to 0
   msg.push_back(0x00); // Index 3: Control Analogues - not implemented, set to 0x00
